98d52be97706bd8373f246ce94c40c3d17f365fd
Emperador24 <148393330+Emperador24@users.noreply.github.com>
2024-08-10 14:55:06 -0500

    Update sepia_filter.cxx

--- sepia_filter.cxx
+++ sepia_filter.cxx
@@ -1,10 +1,21 @@
-#include "sepia_filter.h"
+#include "negative_filter.h"
 
-void SepiaFilter::apply(Image& image) {
+void NegativeFilter::apply(Image& image) {
     std::vector<std::vector<std::vector<int>>> pixels = image.getPixels();
     //-----------------------------------------------------------------------
-    // TODO #6: Implementar el filtro sepia.
-
+    // TODO #5: Implementar el filtro negativo (NEGATIVE).
+    //Declaracion de variables ancho, alto y canales
+    int width = pixels.size();
+    int height = pixels[0].size();
+    int channels = pixels[0][0].size();
+    // Aplicar el filtro negativo
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            for (int c = 0; c < channels; ++c) {
+                pixels[x][y][c] = 255 - pixels[x][y][c];
+            }
+        }
+    }
     //-----------------------------------------------------------------------
     image.setPixels(pixels);
 }
