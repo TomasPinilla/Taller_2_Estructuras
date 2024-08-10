5f6d96a414d7b960b3b6941113162a59a4fd6794
Emperador24 <148393330+Emperador24@users.noreply.github.com>
2024-08-10 14:54:41 -0500

    Update negative_filter.cxx

--- negative_filter.cxx
+++ negative_filter.cxx
@@ -4,7 +4,18 @@ void NegativeFilter::apply(Image& image) {
     std::vector<std::vector<std::vector<int>>> pixels = image.getPixels();
     //-----------------------------------------------------------------------
     // TODO #5: Implementar el filtro negativo (NEGATIVE).
-
+    //Declaracion de variables ancho, alto y canales
+    int width = pixels.size();
+    int height = pixels[0].size();
+    int channels = pixels[0][0].size();
+    // Aplicar el filtro negativo
+    for (int x = 0; x < width; ++x) {
+        for (int y = 0; y < height; ++y) {
+            for (int c = 0; c < channels; ++c) {
+                pixels[x][y][c] = 255 - pixels[x][y][c];
+            }
+        }
+    }
     //-----------------------------------------------------------------------
     image.setPixels(pixels);
 }
